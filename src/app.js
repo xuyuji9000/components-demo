f5b0c0347e6cde1d290e75fe95b28277ffe2153e
yogiman <xuyuji9000@163.com>
2017-01-25 16:39:15 +0800

    first commit

---
+++ src/app.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import NameForm from "./components/NameForm";
+
+
+ReactDOM.render(
+    <NameForm></NameForm>,
+    document.getElementById('root')
+);
